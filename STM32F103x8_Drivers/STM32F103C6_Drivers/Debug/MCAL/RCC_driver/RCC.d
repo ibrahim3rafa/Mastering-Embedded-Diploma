69efc68aa0ced441a8c06eeb9590cb58bcf697c4
Ibrahim Arafa <95101848+ibrahim3rafa@users.noreply.github.com>
2023-08-18 11:49:33 +0300

    Delete STM32F103x8_Drivers directory

--- STM32F103x8_Drivers/STM32F103C6_Drivers/Debug/MCAL/RCC_driver/RCC.d
+++
@@ -1,7 +0,0 @@
-MCAL/RCC_driver/RCC.o: ../MCAL/RCC_driver/RCC.c \
- D:/Embedded/KEROLES/Mastering\ Embedded\ System/github/Mastering-Embedded-Diploma/DRIVERS/STM32F103C6_Drivers/MCAL/includes/RCC.h \
- D:/Embedded/KEROLES/Mastering\ Embedded\ System/github/Mastering-Embedded-Diploma/DRIVERS/STM32F103C6_Drivers/MCAL/includes/STM32F103x6.h
-
-D:/Embedded/KEROLES/Mastering\ Embedded\ System/github/Mastering-Embedded-Diploma/DRIVERS/STM32F103C6_Drivers/MCAL/includes/RCC.h:
-
-D:/Embedded/KEROLES/Mastering\ Embedded\ System/github/Mastering-Embedded-Diploma/DRIVERS/STM32F103C6_Drivers/MCAL/includes/STM32F103x6.h:
